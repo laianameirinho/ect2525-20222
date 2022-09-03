920dcb34133eb1a6482c44e17252d08a19c333f7
labinfo <labinfo@ectufrn.ect.ufrn.br>
2022-09-02 23:52:56 -0300

    commit do vasco

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>VASCO</Text>
+      <Text style={styles.text}>OK OK</Text>
+      <Image
+        
+        style = {styles.img} source = {require('./vasco-da-gama.png')}
+        />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: '#111'
+  },
+  text1: {
+    color: '#111'
+  }, 
+
+  /*img: {
+    wegth: 5,
+    width: 5,
+  }*/
 });
+
