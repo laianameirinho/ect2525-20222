320ef5311dc7efc1473ecc5ad7866872effe3eb4
luiz <luizhenriquecavalcante90@gmail.com>
2022-09-10 00:25:04 -0300

    commit para adicionar projeto

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Image, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text1}>VASCO</Text>
-      <Text style={styles.text}>OK OK</Text>
-      <Image
-        
-        style = {styles.img} source = {require('./vasco-da-gama.png')}
-        />
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Image style={styles.nzinho} source={require('./icones/nzinho.png')}/>
+        <Image style={styles.nzinho} source={require('./icones/perfil.png')}/> 
+      </View>
+
+      <View style={styles.aba}>
+        <Text>Séries</Text>
+        <Text>Filmes</Text>
+        <Text>Categorias</Text>
+      </View>
+
+      <View style={styles.continue}>
+        <Text>Continuar assistindo como Usuario</Text>
+
+      </View>
+        
+      <View style={styles.continuar}>
+        <Image style={styles.foto} source={require('./icones/titanic.png')}/>
+        <Image style={styles.foto} source={require('./icones/star-wars.png')}/>
+        <Image style={styles.foto} source={require('./icones/Friends.png')}/>
+      </View>
+      
     </View>
   );
 }
@@ -19,20 +38,46 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
+  },
+  header: {
+    flexDirection: 'row',
+    height: 60,
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    padding: 5,
   },
 
-  text: {
-    color: '#111'
+  aba: {
+    flexDirection: 'row',
+    height: 60,
+    backgroundColor: '#f00',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 7,
+    },
+    
+  nzinho: {
+    height: 40,
+    width: 40,
   },
-  text1: {
-    color: '#111'
-  }, 
 
-  /*img: {
-    wegth: 5,
-    width: 5,
-  }*/
+  continue: {
+   flexDirection: 'row',
+    height: 16,
+    
+  },
+  continuar: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    padding: 7,
+    alignItems: 'center',
+  },
+
+  foto: {
+    height: 110,
+    width: 90,
+  },
 });
 
